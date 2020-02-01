85af59820a6cdc60024e963340f8aeb98e90873b
unknown <fernandxor@gmail.com>
2020-02-02 00:14:36 +0100

    Su puta madre

--- Unity/Assets/Scripts/CameraFollow.cs
+++ Unity/Assets/Scripts/CameraFollow.cs
@@ -4,38 +4,52 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    [SerializeField]
-    Transform target;
-    Camera camera;
-
-    static CameraFollow cam;
-
+    static CameraFollow instance;
     public static CameraFollow GetInstance() {
-        return cam;
+        return instance;
     }
+
+
+    [SerializeField] Transform target;
+    [SerializeField] float nearZoom = 2, farZoom = 5;
+    Camera cam;
+    float targetSize;
+    Vector2 targetPosition;
 
     private void Awake()
     {
-        cam = this;
-        camera = GetComponent<Camera>();
+        instance = this;
+        cam = GetComponent<Camera>();
+        targetSize = cam.orthographicSize;
     }
  
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = target.position - Vector3.forward * 10;
+
+        targetPosition = target.position - Vector3.forward * 10;
+
+        Vector3 v = Vector2.Lerp(transform.position, targetPosition, Time.deltaTime * 5f);
+        v.z = -10;
+
+        transform.position = v;
+
+        cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, targetSize, Time.deltaTime * 3);
+
     }
 
     public void ZoomIn(Transform target)
     {
         this.target = target;
-        camera.orthographicSize = camera.orthographicSize / 2;
+
+        targetSize = nearZoom;
     }
 
     public void ZoomOut(Transform target = null)
     {
         if (target != null)
             this.target = target;
-        camera.orthographicSize = camera.orthographicSize * 2;
+
+        targetSize = farZoom;
     }
 }
--- Unity/Assets/Scripts/WaterBar.cs
+++ Unity/Assets/Scripts/WaterBar.cs
@@ -1,14 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WaterBar : MonoBehaviour
 {
 
     [SerializeField] Player player;
+    [SerializeField] Image fillerImage;
 
     void Update()
     {
-
+        fillerImage.fillAmount = player.Health / 100f;
     }
 }
