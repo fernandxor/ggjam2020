0ae27a62a15116d5112ea4b17829131e5dd82c95
unknown <fernandxor@gmail.com>
2020-02-02 12:45:26 +0100

    Added HUD things

---
+++ Unity/Assets/Scripts/VisualShit/CarInteractionHUD.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarInteractionHUD : MonoBehaviour
+{
+
+    [SerializeField] Texture2D pointerTexture;
+
+
+    static CarInteractionHUD instance;
+    
+
+    void Awake()
+    {
+        instance = this;
+        Cursor.SetCursor(pointerTexture, Vector2.zero, CursorMode.ForceSoftware);
+        Cursor.visible = false;
+    }
+
+    public static void SetVisible(bool visible)
+    {
+        Cursor.visible = visible;
+    }
+
+}
